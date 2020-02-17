7703e5026adde74cc36b3ee5db1226c217ab7a20
Mateusz Niedbal <mat.niedbal@yahoo.com>
2020-02-17 01:01:24 +0100

    Load file added (working fine)

--- src/main/java/pl/mateusz/niedbal/vector/paint/paint/shapes/Line.java
+++ src/main/java/pl/mateusz/niedbal/vector/paint/paint/shapes/Line.java
@@ -33,36 +33,4 @@ public class Line extends Shape {
         builder.append(strokeColor).append(";");
         return builder.toString();
     }
-
-    public double getStartX() {
-        return startX;
-    }
-
-    public void setStartX(double startX) {
-        this.startX = startX;
-    }
-
-    public double getStartY() {
-        return startY;
-    }
-
-    public void setStartY(double startY) {
-        this.startY = startY;
-    }
-
-    public double getEndX() {
-        return endX;
-    }
-
-    public void setEndX(double endX) {
-        this.endX = endX;
-    }
-
-    public double getEndY() {
-        return endY;
-    }
-
-    public void setEndY(double endY) {
-        this.endY = endY;
-    }
 }
--- src/main/java/pl/mateusz/niedbal/vector/paint/paint/shapes/Rectangle.java
+++ src/main/java/pl/mateusz/niedbal/vector/paint/paint/shapes/Rectangle.java
@@ -43,35 +43,4 @@ public class Rectangle extends Shape {
         return builder.toString();
     }
 
-    public double getStartX() {
-        return startX;
-    }
-
-    public void setStartX(double startX) {
-        this.startX = startX;
-    }
-
-    public double getStartY() {
-        return startY;
-    }
-
-    public void setStartY(double startY) {
-        this.startY = startY;
-    }
-
-    public double getEndX() {
-        return endX;
-    }
-
-    public void setEndX(double endX) {
-        this.endX = endX;
-    }
-
-    public double getEndY() {
-        return endY;
-    }
-
-    public void setEndY(double endY) {
-        this.endY = endY;
-    }
 }
